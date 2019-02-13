427a940d86612e556bac1400a5ae4e37c4618b34
LibLev <libardi.levente@gmail.com>
2019-02-13 13:18:49 +0100

    collect answer

--- data_manager.py
+++ data_manager.py
@@ -4,8 +4,8 @@ import os
 ANSWERS_HEADER = ['id','submission_time','vote_number','question_id','message,image']
 DATA_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
 QUESTIONS = DATA_FILE_PATH = os.getenv(
-    'DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'sample_data/question.csv'
-ANSWERS = DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'sample_data/answer.csv'
+    'DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else '/home/levente/Desktop/web/ask mate/ask-mate-python/sample_data/question.csv'
+ANSWERS = DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else '/home/levente/Desktop/web/ask mate/ask-mate-python/sample_data/answer.csv'
 
 
 def collect_questions():
