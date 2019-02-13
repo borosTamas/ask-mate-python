f1bcec07cd8a5113bf9617e9fd5aef14318847a9
LibLev <libardi.levente@gmail.com>
2019-02-13 14:49:47 +0100

    add answer

--- data_manager.py
+++ data_manager.py
@@ -1,11 +1,12 @@
 import csv
 import os
+import time
 
-ANSWERS_HEADER = ['id','submission_time','vote_number','question_id','message,image']
+ANSWERS_HEADER = ['id','submission_time','vote_number','question_id','message','image']
 DATA_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
 QUESTIONS = DATA_FILE_PATH = os.getenv(
-    'DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'sample_data/question.csv'
-ANSWERS = DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'sample_data/answer.csv'
+    'DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else '/home/levente/Desktop/web/ask mate/ask-mate-python/sample_data/question.csv'
+ANSWERS = DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else '/home/levente/Desktop/web/ask mate/ask-mate-python/sample_data/answer.csv'
 
 
 def collect_questions():
@@ -16,12 +17,15 @@ def collect_questions():
             result.append(question)
         return result
 
+
 def find_question(id):
     list_of_questions = collect_questions()
     for question in list_of_questions:
         if question['id'] == id:
             result = question
+            print(result["title"])
             return result
+
 
 def collect_answers():
     with open(ANSWERS, 'r') as answers:
@@ -30,3 +34,17 @@ def collect_answers():
         for answers in answers_dict:
             result.append(answers)
         return result
+
+
+def add_answer(form_data):
+    new_id = collect_answers()[-1]['id']
+    if new_id == 'id':
+        new_id = 1
+    else:
+        new_id = int(new_id) + 1
+    submission_time = int(time.time())
+    prepared_data = [item for key, item in form_data.items()]
+    prepared_data = list(str(new_id)+str(submission_time)+str(''))+prepared_data
+    with open(ANSWERS, 'a', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(prepared_data)
--- server.py
+++ server.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template,request
 import data_manager
 app = Flask(__name__)
 
+saved_data = {}
+
 @app.route('/')
 def render_index():
     questions = data_manager.collect_questions()
@@ -12,6 +14,17 @@ def render_index():
 def show_question(id):
     question = data_manager.find_question(id)
     return render_template('question_page.html', question=question)
+
+
+@app.route('/question_page/<id>/new-answer', methods=['GET','POST'])
+def post_an_answer(id):
+    if request.method == 'POST':
+        saved_data['question_id'] = request.form['question']
+        saved_data['message'] = request.form['message']
+        saved_data['image'] = request.form['image']
+        data_manager.add_answer(saved_data)
+        return redirect('/question_page/<id>')
+    return  render_template('new_answer.html', question=question)
 
 
 @app.route('/add-question',methods=['GET','POST'])
