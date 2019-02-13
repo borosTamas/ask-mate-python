cc6e6bbf78ce8d9ca3277d753c1fffe6d6fa2d90
borosTamas <ikuto19930802@gmail.com>
2019-02-13 11:34:39 +0100

    commit before the lunch just for safe

--- data_manager.py
+++ data_manager.py
@@ -1,10 +1,13 @@
 import csv
+import os
 
-DATA_HEADER = ['id','submission_time','view_number','vote_number','title','message','image']
-QUESTIONS = 'sample_data/question.csv'
-ANSWERS = 'sample_data/answer.csv'
+DATA_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+QUESTIONS = DATA_FILE_PATH = os.getenv(
+    'DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'sample_data/question.csv'
+ANSWERS = DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'sample_data/answer.csv'
 
-def collect_qestions():
+
+def collect_questions():
     with open(QUESTIONS, 'r') as questions:
         result = []
         questions_dict = csv.DictReader(questions, fieldnames=DATA_HEADER)
@@ -12,6 +15,9 @@ def collect_qestions():
             result.append(question)
         return result
 
+def find_question(id,list_of_questions):
+    for question in list_of_questions:
+        pass
 
 def collect_answers():
     pass
--- server.py
+++ server.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def render_index():
-    questions = data_manager.collect_qestions()
+    questions = data_manager.collect_questions()
     return render_template('index.html', questions=questions)
+
+@app.route('/question_page/<id>')
+def show_question(id):
+    question = question_data
+    return render_template('question_page/<id>', question=question)
 
 
 if __name__=="__main__":
