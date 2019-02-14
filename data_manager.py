915b2336082fdba6d1ae0c1d829a9b5138c51418
LibLev <libardi.levente@gmail.com>
2019-02-14 12:47:48 +0100

    dani belenyult

--- data_manager.py
+++ data_manager.py
@@ -2,20 +2,21 @@ import csv
 import os
 import time
 
-ANSWERS_HEADER = ['id','submission_time','vote_number','question_id','message','image']
+ANSWERS_HEADER = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
 DATA_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
-QUESTIONS = DATA_FILE_PATH = os.getenv(
-    'DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else '/home/levente/Desktop/web/ask mate/ask-mate-python/sample_data/question.csv'
-ANSWERS = DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else '/home/levente/Desktop/web/ask mate/ask-mate-python/sample_data/answer.csv'
+
+QUESTIONS_FILE_PATH = os.getenv('QUESTIONS_FILE_PATH', 'sample_data/question.csv')
+ANSWERS_FILE_PATH = os.getenv('ANSWERS_FILE_PATH', 'sample_data/answer.csv')
 
 
 def collect_questions():
-    with open(QUESTIONS, 'r') as questions:
+    with open(QUESTIONS_FILE_PATH, 'r') as questions:
         result = []
         questions_dict = csv.DictReader(questions, fieldnames=DATA_HEADER)
         for question in questions_dict:
             result.append(question)
         return result
+
 
 def find_question(id):
     list_of_questions = collect_questions()
@@ -24,15 +25,16 @@ def find_question(id):
             result = question
             return result
 
+
 def collect_answers():
-    with open(ANSWERS, 'r') as answers:
+    with open(ANSWERS_FILE_PATH, 'r') as answers:
         result = []
         answers_dict = csv.DictReader(answers, fieldnames=ANSWERS_HEADER)
         for answers in answers_dict:
             if answers['question_id'] == id:
                 result.append(answers)
             else:
-                result = [{'message':'There is no answers yet.'}]
+                result = [{'message': 'There is no answers yet.'}]
         return result
 
 
@@ -44,7 +46,7 @@ def add_answer(form_data):
         new_id = int(new_id) + 1
     submission_time = int(time.time())
     prepared_data = [item for key, item in form_data.items()]
-    prepared_data = list(str(new_id)+str(submission_time)+str(''))+prepared_data
-    with open(ANSWERS, 'a', newline='') as csv_file:
+    prepared_data = list(str(new_id) + str(submission_time) + str('')) + prepared_data
+    with open(ANSWERS_FILE_PATH, 'a', newline='') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(prepared_data)
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request, redirect
 import data_manager
 app = Flask(__name__)
 
@@ -13,19 +13,21 @@ def render_index():
 @app.route('/question_page/<id>')
 def show_question(id):
     question = data_manager.find_question(id)
-    answers = data_manager.collect_answers(id)
+    answers = data_manager.collect_answers()
     return render_template('question_page.html', question=question, answers=answers)
 
 
 @app.route('/question_page/<id>/new-answer', methods=['GET','POST'])
 def post_an_answer(id):
+    question = data_manager.find_question(id)
+    answer = data_manager.collect_answers()
     if request.method == 'POST':
         saved_data['question_id'] = request.form['question']
         saved_data['message'] = request.form['message']
         saved_data['image'] = request.form['image']
         data_manager.add_answer(saved_data)
         return redirect('/question_page/<id>')
-    return  render_template('new_answer.html', question=question)
+    return render_template('new_answer.html', question=question, answer=answer)
 
 
 @app.route('/add-question',methods=['GET','POST'])
